bb3a338b14303ffefe278d59baf072d522af5c08
Toufiq A. Shishir <toufiq_a24@outlook.com>
2025-05-15 01:09:36 +0600

    feat: implemented api views for auth & user management for `UserAccount`

---
+++ apps/users/views.py
@@ -0,0 +1,153 @@
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.generics import GenericAPIView, RetrieveUpdateAPIView
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from apps.users.serializers import (
+    UserSerializer,
+    UserRegisterSerializer,
+    UserLoginSerializer,
+    ProfileSerializer,
+    ProfileAvatarSerializer,
+)
+from apps.users.models import Profile
+
+
+User = get_user_model()
+
+
+class UserRegisterAPIView(GenericAPIView):
+    serializer_class = UserRegisterSerializer
+    permission_classes = (AllowAny,)
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token = RefreshToken.for_user(user)
+        data = serializer.data
+        data["tokens"] = {
+            "refresh": str(token),
+            "access": str(token.access_token),
+        }
+        response = Response(data, status=status.HTTP_201_CREATED)
+        # set http-only cookie
+        response.set_cookie(
+            key="access_token",
+            value=str(token.access_token),
+            httponly=True,
+            samesite="None",
+            secure=True,
+        )
+        response.set_cookie(
+            key="refresh_token",
+            value=str(token),
+            httponly=True,
+            samesite="None",
+            secure=True,
+        )
+        return response
+
+
+class UserLoginAPIView(GenericAPIView):
+    serializer_class = UserLoginSerializer
+    permission_classes = (AllowAny,)
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        serializer = UserSerializer(user)
+        token = RefreshToken.for_user(user)
+        data = serializer.data
+        data["tokens"] = {
+            "refresh": str(token),
+            "access": str(token.access_token),
+        }
+        response = Response(data, status=status.HTTP_200_OK)
+        # set http-only cookie
+        response.set_cookie(
+            key="access_token",
+            value=str(token.access_token),
+            httponly=True,
+            samesite="None",
+            secure=True,
+        )
+        response.set_cookie(
+            key="refresh_token",
+            value=str(token),
+            httponly=True,
+            samesite="None",
+            secure=True,
+        )
+        return response
+
+
+class UserLogoutAPIView(GenericAPIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        try:
+            refresh_token = request.COOKIES.get("refresh_token")
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(
+                {"detail": "Logout successful"}, status=status.HTTP_205_RESET_CONTENT
+            )
+        except Exception as e:
+            return Response(
+                {"detail": "Logout failed", "error": f"{str(e)}"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+
+class TokenRefreshAPIView(TokenRefreshView):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        if response.status_code == 200:
+            access_token = response.data["access"]
+            refresh_token = response.data["refresh"]
+            response.set_cookie(
+                key="access_token",
+                value=access_token,
+                httponly=True,
+                samesite="None",
+                secure=True,
+            )
+            response.set_cookie(
+                key="refresh_token",
+                value=refresh_token,
+                httponly=True,
+                samesite="None",
+                secure=True,
+            )
+        return response
+
+
+class UserAPIView(RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
+
+
+class UserProfileAPIView(RetrieveUpdateAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user.profile
+
+
+class UserProfileAvatarAPIView(RetrieveUpdateAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileAvatarSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user.profile
