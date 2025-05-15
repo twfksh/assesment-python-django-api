3721d65d1c2e78fd6caa0e52fbe7b73e75380670
Toufiq A. Shishir <toufiq_a24@outlook.com>
2025-05-15 10:43:50 +0600

    feat: register Tag, Post, and Comment models in admin

--- apps/posts/admin.py
+++ apps/posts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.posts.models import Tag, Post, Comment
+
+
+admin.site.register(Tag)
+admin.site.register(Post)
+admin.site.register(Comment)
